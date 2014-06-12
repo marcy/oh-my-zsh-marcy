fce54e43696735fbf9d30ad4b4ee11c2350e55d5
marcy <marcy8468@gmail.com>
2014-06-12 13:53:21 +0900

    do_enter

--- my.zshrc.zsh
+++ my.zshrc.zsh
@@ -63,6 +63,7 @@ function do_enter() {
     # ls を表示
     echo
     ls
+    echo
     # git status を表示
     # ls_abbrev
     if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
